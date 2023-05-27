c76b6b6ee94961ac22a0324cb5ab2153e14e5120
Tashfiq Chowdhury <tshfq.chw@gmail.com>
2023-05-27 16:27:15 -0700

    added endpoints for gettings by multiple ids

--- src/routes/contentRoute.ts
+++ src/routes/contentRoute.ts
@@ -5,6 +5,9 @@ import {
   getSubtopicsHandler,
   getTextbookByIdHandler,
   getAllTextbooksHandler,
+  getChaptersByIdsHanbler,
+  getSubchaptersByIdsHanbler,
+  getSubtopicsByIdsHandler,
 } from "../controllers/contentController";
 import verifyToken from "../middleware/authMiddleware";
 const contentRoute = express.Router();
@@ -14,8 +17,15 @@ contentRoute.use(verifyToken);
 
 contentRoute.get("/textbooks", getAllTextbooksHandler);
 contentRoute.get("/textbooks/:textbookId", getTextbookByIdHandler);
+// need to remove the below route
 contentRoute.get("/textbooks/:textbookId/chapters", getChaptersHandler);
+// need to keep the route below
+contentRoute.get("/chapters/:chapterId", getChaptersHandler);
+contentRoute.get("/chapters", getChaptersByIdsHanbler);
+// add an s after all these for consistenncy
 contentRoute.get("/subchapter/:chapterId", getSubchaptersHandler);
+contentRoute.get("/subchapters", getSubchaptersByIdsHanbler);
 contentRoute.get("/subtopic/:subchapterId", getSubtopicsHandler);
+contentRoute.get("/subtopics", getSubtopicsByIdsHandler);
 
 export default contentRoute;
